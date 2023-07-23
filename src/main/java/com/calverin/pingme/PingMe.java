5ffb39e6d1a5cf6c38f8d08bd8d930f8a5f0368c
Calverin <yosefs646@gmail.com>
2023-07-23 02:48:29 -0400

    Added support for @ after name for autocomplete

--- src/main/java/com/calverin/pingme/PingMe.java
+++ src/main/java/com/calverin/pingme/PingMe.java
@@ -32,11 +32,14 @@ public class PingMe extends JavaPlugin implements Listener {
         List<Player> players = new ArrayList<>(getServer().getOnlinePlayers());
         String msg = event.getMessage();
         for (Player player : players) {
-            if (msg.contains("@" + player.getName()) || msg.contains("@" + player.getName().toLowerCase()) || msg.contains("@" + player.getName().toUpperCase())) {
+            if (msg.contains("@" + player.getName()) || msg.contains("@" + player.getName().toLowerCase()) || msg.contains("@" + player.getName().toUpperCase()) || msg.contains(player.getName() + "@") || msg.contains(player.getName().toLowerCase() + "@") || msg.contains(player.getName().toUpperCase() + "@")) {
                 player.playSound(player.getLocation(), "entity.experience_orb.pickup", 100, 0);
                 msg = msg.replaceAll("@" + player.getName(), "§e@" + player.getName() + "§r");
                 msg = msg.replaceAll("@" + player.getName().toLowerCase(), "§e@" + player.getName().toLowerCase() + "§r");
                 msg = msg.replaceAll("@" + player.getName().toUpperCase(), "§e@" + player.getName().toUpperCase() + "§r");
+                msg = msg.replaceAll(player.getName() + "@", "§e@" + player.getName() + "§r");
+                msg = msg.replaceAll(player.getName().toLowerCase() + "@", "§e@" + player.getName().toLowerCase() + "§r");
+                msg = msg.replaceAll(player.getName().toUpperCase() + "@", "§e@" + player.getName().toUpperCase() + "§r");
                 //player.sendTitle("\u00A7d\u00A7l" + event.getPlayer().getName() + "\u00A7r\u00A7e pinged you!", msg, 2, 35, 3);
                 player.sendTitle("", msg, 2, 35, 3);
             }
