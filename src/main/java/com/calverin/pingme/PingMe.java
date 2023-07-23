5d697ea65b1058b33179a7ea9dee3b08207b5a0c
Calverin <yosefs646@gmail.com>
2023-07-23 02:28:41 -0400

    Working ping plugin

--- src/main/java/com/calverin/pingme/PingMe.java
+++ src/main/java/com/calverin/pingme/PingMe.java
@@ -1,17 +1,47 @@
 package com.calverin.pingme;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.*;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class PingMe extends JavaPlugin {
+public class PingMe extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        getLogger().info("onEnable is called!");
+        getLogger().info("Using PingMe by Calverin!");
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("onDisable is called!");
+        getLogger().info("No longer using PingMe by Calverin!");
+    }
+
+    //@EventHandler
+    //public void onPlayerJoin(PlayerJoinEvent event) {
+    //    event.setJoinMessage("Welcome to the server! :D");
+    //}
+
+    @EventHandler
+    public void onPlayerChat(AsyncPlayerChatEvent event) {
+        List<Player> players = new ArrayList<>(getServer().getOnlinePlayers());
+        String msg = event.getMessage();
+        for (Player player : players) {
+            if (msg.contains("@" + player.getName()) || msg.contains("@" + player.getName().toLowerCase()) || msg.contains("@" + player.getName().toUpperCase())) {
+                player.playSound(player.getLocation(), "entity.experience_orb.pickup", 100, 0);
+                msg = msg.replaceAll("@" + player.getName(), "§e@" + player.getName() + "§r");
+                msg = msg.replaceAll("@" + player.getName().toLowerCase(), "§e@" + player.getName().toLowerCase() + "§r");
+                msg = msg.replaceAll("@" + player.getName().toUpperCase(), "§e@" + player.getName().toUpperCase() + "§r");
+                //player.sendTitle("\u00A7d\u00A7l" + event.getPlayer().getName() + "\u00A7r\u00A7e pinged you!", msg, 2, 35, 3);
+                player.sendTitle("", msg, 2, 35, 3);
+            }
+        }
+        event.setMessage(msg);
     }
     
 }
